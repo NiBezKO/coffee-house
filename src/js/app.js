10d6beaaa59aa9d6eb1170233070539d18b37986
NiBezKO <Rezek32rus@gmail.com>
2022-11-10 00:24:18 +0300

    add media requires and scropts

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,30 @@
+/* add header class */
+
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+  let scrollPos = window.scrollY;
+  
+  if (scrollPos >= 250) {
+    header.classList.add ("_black");
+  } else {
+    header.classList.remove ("_black");
+  }
+}) 
+
+/* Burger menu */
+
+const burger = document.querySelector(".burger");
+const burgerContent = document.querySelector(".burger__content");
+const nav = document.querySelector(".nav");
+
+burger.addEventListener("click", () => {
+  burger.classList.toggle("_active");
+  nav.classList.toggle("_active");
+  burgerContent.classList.toggle("_active");
+});
+
+
 /* slider-swiper */
 
 var swiper = new Swiper(".mySwiper", {
